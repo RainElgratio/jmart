6845d969f0eea835824b560886b9de5e8614f645
unknown <rainsionlumbangaol24@gmail.com>
2021-09-18 08:06:39 +0700

    initial modul 1 -revisi

--- rainJmartFH/Jmart.java
+++ rainJmartFH/Jmart.java
@@ -1,51 +1,64 @@
 package rainJmartFH;
 
 
-/**
- * Write a description of class Jmart here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Jmart main (String[]):void
-{
-    public int x;
-    public int before, after;
+public class Jmart{
+    public static void main(String[] args){
+        System.out.print("program berjalan");
+    }
     
-    getPromo(x)
-    {
+    int getPromo(){
         return 0;
     }
     
-    getCustomer():String
-    {
-        return "oop"
+    String getCustomer(){
+        return "oop";
     }
-    getDiscountPercentage(int,int):float
-    getDiscountPrice(int,float):int
-    getOriginalPrice(int,float):int
-    getCommissionMultiplier():float
-    getAdjustedPrice(int):int
-    getAdminFree(int):int
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
+    
+    float getDiscountPercentage(int before, int after){
+        float percentage;
+        
+        if(before < after){
+            return 0;
+        }
+        else{
+            percentage = 100 - ((after/before) * 100);
+            return percentage;
+        }
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    int getDiscountedPrice(int price, float discountPercentage){
+        int result = price * (discountPercentage/100);
+        
+        if(discountPercentage > 100){
+            return 0;
+        }
+        else{
+            return result;
+        }
+    }
+    
+    int getOriginalPrice(int discountedprice, float discountPercentage){
+        return discountedPrice/(1 - discountPercentage);
+    }
+    
+    float getCommissionMultyplier(){
+        return 0.05;
+    }
+    
+    int getAdjustedPrice(int price){
+        return (price*0.05) + price;
+    }
+    
+    int getAdminFee(int price){
+        return price*0.05;
+    }
+}
+        
+        if(discountPercentage > 100){
+            return 0;
+        }
+        else{
+            return result;
+        }
     }
 }
