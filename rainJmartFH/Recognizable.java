cb5a507968b34f7f86f2dbf950488f515fd9404c
unknown <rainsionlumbangaol24@gmail.com>
2021-09-25 11:00:11 +0700

    Modul 3 - Rain Elgratio - Case Study

---
+++ rainJmartFH/Recognizable.java
@@ -0,0 +1,25 @@
+package rainJmartFH;
+
+
+/**
+ * Write a description of class Recognizeable here.
+ *
+ * @author rain
+ * @version (a version number or a date)
+ */
+public class Recognizable
+{
+    // instance variables - replace the example below with your own
+    public int id;
+    protected Recognizeable(int id){
+        this.id = id;
+    }
+    
+    public boolean equals(Object id){
+        return null;
+    }
+    public boolean equals(Recognizable id){
+        return null;
+    }
+    
+}
